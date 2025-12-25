63c1679495a8e188087072e3ccce400e0a47f21a
AleksMercer <Kalagaev.Alex.A@gmail.com>
2025-12-25 14:30:25 +0300

    task-6

--- app.js
+++ app.js
@@ -1,57 +1,87 @@
-const express = require("express");
-const axios = require("axios");
-const pug = require("pug");
+export default (express, bodyParser, createReadStream, crypto, http) => {
+  const CORS_HEADERS = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,POST,PUT,PATCH,OPTIONS,DELETE",
+    "Access-Control-Allow-Headers": "*",
+  };
 
-const app = express();
+  const TEXT_PLAIN_HEADER = { "Content-Type": "text/plain; charset=utf-8" };
+  const SYSTEM_LOGIN = "83d8909a-b053-40bc-b4cd-4268e60b19b3";
 
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "*");
-  next();
-});
+  const corsMiddleware = (req, res, next) => {
+    for (const [header, value] of Object.entries(CORS_HEADERS)) {
+      res.setHeader(header, value);
+    }
+    if (req.method === "OPTIONS") return res.sendStatus(204);
+    next();
+  };
 
-const login = "83d8909a-b053-40bc-b4cd-4268e60b19b3";
-app.get("/login/", (_, res) => {
-  res.send(login);
-});
+  const ensureTrailingSlash = (req, res, next) => {
+    if (!req.path.endsWith("/") && req.path.length > 1) {
+      const query = req.url.slice(req.path.length);
+      return res.redirect(301, `${req.path}/${query}`);
+    }
+    next();
+  };
 
-app.get("/wordpress/wp-json/wp/v2/posts/1", (_, res) => {
-  res.json({
-    id: 1,
-    slug: login,
-    title: {
-      rendered: login,
-    },
-    content: {
-      rendered: "",
-      protected: false,
-    },
+  const app = express();
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(corsMiddleware);
+  app.use(ensureTrailingSlash);
+
+  app.get("/login/", (_, res) => {
+    res.set(TEXT_PLAIN_HEADER).send(SYSTEM_LOGIN);
   });
-});
 
-app.use(express.json());
+  app.get("/code/", async (_, res) => {
+    try {
+      const filePath = new URL(import.meta.url).pathname;
+      const normalizedPath =
+        process.platform === "win32" ? filePath.substring(1) : filePath;
 
-app.post("/render/", async (req, res) => {
-  const { random2, random3 } = req.body;
-  const { addr } = req.query;
+      const readStream = createReadStream(normalizedPath);
+      let data = "";
+      for await (const chunk of readStream) data += chunk;
 
-  try {
-    const templateResponse = await axios.get(addr);
-    const pugTemplate = templateResponse.data;
+      res.set(TEXT_PLAIN_HEADER).send(data);
+    } catch (error) {
+      res.status(500).send("Error reading file");
+    }
+  });
 
-    const compiled = pug.compile(pugTemplate);
-    const html = compiled({ random2, random3 });
+  app.get("/sha1/:input/", (req, res) => {
+    const hash = crypto
+      .createHash("sha1")
+      .update(req.params.input)
+      .digest("hex");
+    res.set(TEXT_PLAIN_HEADER).send(hash);
+  });
 
-    res.set("Content-Type", "text/html");
-    res.send(html);
-  } catch (error) {
-    console.error(error);
-    res.status(500).send("Error processing template");
-  }
-});
+  const handleReq = async (addr, res) => {
+    try {
+      return new Promise((resolve, reject) => {
+        http
+          .get(addr, (response) => {
+            let data = "";
+            response.on("data", (chunk) => (data += chunk));
+            response.on("end", () =>
+              resolve(res.set(TEXT_PLAIN_HEADER).send(data))
+            );
+          })
+          .on("error", reject);
+      });
+    } catch (error) {
+      res.status(500).send(error.toString());
+    }
+  };
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  app.get("/req/", (req, res) => handleReq(req.query.addr, res));
+  app.post("/req/", (req, res) => handleReq(req.body.addr, res));
+
+  app.all("*", (_, res) => {
+    res.set(TEXT_PLAIN_HEADER).send(SYSTEM_LOGIN);
+  });
+
+  return app;
+};
---
+++ index.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { createReadStream } from "fs";
+import crypto from "crypto";
+import http from "http";
+import bodyParser from "body-parser";
+
+import appSrc from "./app.js";
+
+const app = appSrc(express, bodyParser, createReadStream, crypto, http);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
